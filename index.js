17dad4207b34a687a1f51c9afac65251420119dc
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-23 19:12:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your classes here
+class Vehicle {
+  constructor( make ) {
+    this._make = make
+  }
+ 
+  get make() {
+    return this._make
+  }
+ 
+ static definition() {
+    return 'A vehicle is used for transporting people or goods, especially on land.'
+  }
+}
+ 
+class Car extends Vehicle {
+  constructor( make , driveTrain ) {
+    super( make )
+    this._driveTrain = driveTrain
+  }
+ 
+  static definition() {
+    return `${super.definition()} A car is a road vehicle, typically with four wheels, powered by an internal combustion engine and able to carry a small number of people.`
+  }
+}
