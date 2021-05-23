4b4683f83d43edf3df73f95503f65abbcc001241
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-23 19:16:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,15 @@ class Deciduous extends Tree {
     return `${super.definition()} Deciduous trees shed their leaves annually.`
   }
 }
+
+
+class Deciduous extends Tree {
+  constructor( species , name ) {
+    super( species )
+    this.name = name
+  }
+ 
+  static definition() {
+    return `${super.definition()} Deciduous trees shed their leaves annually.`
+  }
+}
